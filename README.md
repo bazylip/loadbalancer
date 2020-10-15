ac781e0f263942f5711f41e0733373be84b1507b
Debskij <36741649+Debskij@users.noreply.github.com>
2020-10-15 19:04:40 +0200

    Fix network from containers, add webscraping (#4)
    Co-authored-by: BazyliP <bazyli.polednia@gmail.com>

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Instructions
 
  - Pull repository to your workspace
+ - Change default DNS servers
+     - `sudo ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf`
  - Build docker images: 
      - `docker-compose build --no-cache`
  - Bring up the containers: 
      - `docker-compose up`
  - To test the connection:
-     - `localhost:8080` 
+     - `localhost:8080`
+ - Run load generator:
+     - `python3 tools/load_generator/load_generator.py -a NGROK_ADDRESS -n 5`
--- dockerfiles/server/app.py
+++ dockerfiles/server/app.py
@@ -1,11 +1,18 @@
 from flask import request, Flask
-import json
+import requests
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello world"
+    session = HTMLSession()
+    response = session.get("https://weather.com/en-IN/weather/tenday/l/8bc076d687d001f1e0e56eee869718d3b71f50bb7562ff6323d0d7547a8812c6")
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    temp = soup.find('span', {'data-testid': 'TemperatureValue'}).text
+    return f"Current temperature in Gdansk: {temp}"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
